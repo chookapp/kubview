c6104f94bc8af2cc1bf77f4eb3e29bf364f694c9
Gil Barash <gil.barash@zerto.com>
2020-01-07 12:51:15 +0200

    [master] very init works
    Branch: [master]

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,75 +1,154 @@
 import React from 'react';
 import k8sproxy from './k8sproxy_mock.js';
 
+function ItemStatus(props) {
+    return (
+    //<div style={{display: "inline-block;"}}>{props.item.status}</div>
+    <span>{props.item.status}</span>
+    );
+}
+
+function ItemName(props) {
+    return (
+    <span>{props.item.name}</span>
+    );
+}
+
+
+function Pod(props) {
+    const pod = props.pod;
+    
+    return (
+    <div> {pod.kind} <ItemName item={pod}/> <ItemStatus item={pod}/></div>
+    );
+  }
+
+
+function StatefullSet(props) {
+    const ss = props.ss;
+    const pods = ss.pods.map((pod) =>
+    <li><Pod pod={pod}/></li>
+    );
+    return (
+      <div> {ss.kind} <ItemName item={ss}/> <ItemStatus item={ss}/>
+      <ul>{pods}</ul>
+      </div>
+    );
+  }
+
 class MainScreen extends React.Component {
 
     constructor(props) {
-        super(props);
-
-        this.data = {
-        };
+        super(props);     
 
         this.state =
         {
-            pods: {},
-            nodes: {},
-            namespaces: {},
-            pvs: {},
-            pvcs: {},
-            statefullsetes: {},
-            deployments: {},
+            data:
+            {
+                pods: [],
+                nodes: [],
+                namespaces: [],
+                pvs: [],
+                pvcs: [],
+                statefullsetes: [],
+                deployments: [],
+            }
         }
+
+//        this.calcNewState = this.calcNewState.bind(this);
     }
 
     componentDidMount() {
         const k8p = new k8sproxy();
         let promises = [];
+        let myData = {};
 
-        promises.push(k8p.listPodForAllNamespaces().then((data) => { this.data.pods = this.createItems(data) }));
-        promises.push(k8p.listPersistentVolumeClaimForAllNamespaces().then((data) => { this.data.pvcs = this.createItems(data) }));
+        promises.push(k8p.listPodForAllNamespaces().then((data) => { myData.pods = this.createItems(data) }));
+        promises.push(k8p.listPersistentVolumeClaimForAllNamespaces().then((data) => { myData.pvcs = this.createItems(data) }));
+        promises.push(k8p.listPersistentVolumeForAllNamespaces().then((data) => { myData.pvs = this.createItems(data) }));
+        promises.push(k8p.listStatefulSetForAllNamespaces().then((data) => { myData.statefullsetes = this.createItems(data) }));
 
-        Promise.all(promises).then(() => { this.calcNewState() });
+        Promise.all(promises).then(() => { this.setState({data: myData}); });
     }
 
     createItems(data) {
-        for (let item in data.items) {
+        let ret = []
+        for (let item of data.items) {
             let newItem = {}
             newItem.kind = item.kind
             newItem.name = item.metadata.name
             newItem.namespace = item.metadata.namespace
+            newItem.id = item.metadata.uid
             newItem.status = item.status.phase
 
-            if (newItem.kind == "Pod") {
+            if (newItem.kind === "StatefulSet") {                                
+                newItem.status = item.status.readyReplicas + "/" + item.status.replicas
+            }
+
+            if (newItem.kind === "Pod") {
                 newItem.node = item.spec.nodeName
+                newItem.ownerId = item.metadata.ownerReferences.uid
                 newItem.vols = []
-                for (let vol in item.spec.volumes) {
+                for (let vol of item.spec.volumes) {
                     if ("persistentVolumeClaim" in vol) {
                         newItem.vols.push(vol.persistentVolumeClaim.claimName)
                     }
                 }
             }
 
-            if (newItem.kind == "PersistentVolumeClaim") {
+            if (newItem.kind === "PersistentVolumeClaim") {
                 // newItem.size = item.spec.resources.requests.storage
                 newItem.pv = item.spec.volumeName
             }
 
-            if (newItem.kind == "PersistentVolume") {
+            if (newItem.kind === "PersistentVolume") {
                 newItem.size = item.spec.capacity.storage
-            }
+            }   
+            ret.push(newItem)
         }
+        return ret
     }
 
-    calcNewState() {
-        this.setState({ pods: this.data.pods });
+    getChildPods(id, pods) {
+        let ret = [];
+        let remains = [];
+        for(let pod of pods) {
+            if(pod.ownerId === id)
+                ret.push(pod)
+            else
+                remains.push(pod)
+        }  
+
+        return [ret, remains];
     }
+
 
     render() {
 
-        return (
-            <h2>Gil
-            {Object.keys(this.state.pods).length}
-            </h2>
+        //let pvcs = this.state.data.pvcs;
+
+
+
+        let pods = this.state.data.pods;
+
+
+        let statefullsetes = this.state.data.statefullsetes;
+        for(let ss of statefullsetes) {
+            const [children, remains] = this.getChildPods(ss.id, pods);
+            pods = remains
+            ss.pods = children
+        }
+
+
+        
+
+        // return (
+        //     <h2>Gil
+        //     {Object.keys(this.state.pods).length}
+        //     </h2>
+        // );
+        return(
+            <ul>{statefullsetes.map((ss) => <li><StatefullSet ss={ss}/></li>)}</ul>
         );
     }
 }
--- src/k8sproxy_mock.js
+++ src/k8sproxy_mock.js
@@ -3,15 +3,38 @@ const axios = require('axios');
 class k8sproxy {
 
     listPodForAllNamespaces() {
-            return axios.get('../testdata/pod.json').then((response) => {
-                return(response.data);
-            }).catch(function (error) {
-                console.log(error);
-            })
-        }
-
-
+        return axios.get('../testdata/pod.json').then((response) => {
+            return(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        })
     }
+
+    listPersistentVolumeClaimForAllNamespaces() {
+        return axios.get('../testdata/pvc.json').then((response) => {
+            return(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    listPersistentVolumeForAllNamespaces() {
+        return axios.get('../testdata/pv.json').then((response) => {
+            return(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    listStatefulSetForAllNamespaces() {
+        return axios.get('../testdata/statefulset.json').then((response) => {
+            return(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+}
 
 
 
